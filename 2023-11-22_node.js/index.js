7b4c1b95f8e8582f0c465dcf2666cc9e44e5ecac
borossandor27 <boros.sanyi@gmail.com>
2023-11-22 11:03:09 +0100

    adatbazissal

--- 2023-11-22_node.js/index.js
+++ 2023-11-22_node.js/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express(); //-- http szervert tududnk vele indítani
 
+//-- átirányítások kezelése miatt kell
+const cors = require('cors');
+app.use(cors());
+
 // JSON adatok fogadása miatt kell
 const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
@@ -8,6 +12,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+const mysql = require('mysql');
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'tagdij'
+    }
+);
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -33,6 +50,20 @@ app.post('/bela', (req, res) => {
     res.send(`Ez Béla lapja, id: ${id}, name: ${name}`);
 });
 
+app.get('/tagok', (req, res) => {
+    let sqlcommand = 'SELECT * FROM `ugyfel`';
+    db.query(sqlcommand, (err, rows) => {
+        if (err) throw err;
+        res.send(rows);
+    });
+});
+app.get('/tagok/:id', (req, res) => {
+    let sqlcommand = `SELECT * FROM ugyfel WHERE azon=${req.params.id}`;
+    db.query(sqlcommand, (err, rows) => {
+        if (err) throw err;
+        res.send(rows);
+    });
+});
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
